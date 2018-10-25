8441ac829dccca674943a719600eadf23e9268d2
Konrad Mikucki <komik966@gmail.com>
2018-10-25 21:32:29 +0200

    trainee registry

--- src/test/kotlin/Stepdefs.kt
+++ src/test/kotlin/Stepdefs.kt
@@ -5,13 +5,42 @@ import cucumber.api.java8.Pl
 class Stepdefs : Pl {
 
     init {
-        Gdy("wpiszę w pole wartość {string}") { string: String ->
+        Jeżeli("jestem \"([^\"]*)\"") {
+        }
+        Gdy("gdy spróbuję dodać nowego trenera z danymi:") { dataTable: io.cucumber.datatable.DataTable ->
+            // Write code here that turns the phrase above into concrete actions
+            // For automatic transformation, change DataTable to one of
+            // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+            // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+            // Double, Byte, Short, Long, BigInteger or BigDecimal.
+            //
+            // For other transformations you can register a DataTableType.
+            throw cucumber.api.PendingException()
         }
 
-        Zakładając("że w rejestrze istnieje trener, którego umowa kończy się {string}") { string: String ->
+        Wtedy("nowy trener powinien pojawić się na liście") {
+            // Write code here that turns the phrase above into concrete actions
+            throw cucumber.api.PendingException()
         }
 
-        Wtedy("menadżer powinien otrzymać przypomnienie w dniu {string}") { string: String ->
+        Wtedy("nowy trener powinien pojawić się w kalendarzu dostępności") {
+            // Write code here that turns the phrase above into concrete actions
+            throw cucumber.api.PendingException()
+        }
+
+        Wtedy("dostępność trenera jest maksymalnie do \"([^\"]*)\"") { string: String ->
+            // Write code here that turns the phrase above into concrete actions
+            throw cucumber.api.PendingException()
+        }
+
+        Zakładając("w rejestrze istnieje trener, którego umowa kończy się \"([^\"]*)\"") { string: String ->
+            // Write code here that turns the phrase above into concrete actions
+            throw cucumber.api.PendingException()
+        }
+
+        Wtedy("powinienem otrzymać przypomnienie w dniu \"([^\"]*)\"") { string: String ->
+            // Write code here that turns the phrase above into concrete actions
+            throw cucumber.api.PendingException()
         }
     }
 }
--- src/test/resources/features/resources_index.feature
+++ src/test/resources/features/resources_index.feature
@@ -2,16 +2,27 @@
 Funkcja: Rejestr trenerów
 
   Scenariusz: Dodawanie nowego trenera
-    Zakładając że jestem menadżerem
-    Gdy gdy spróbuję dodać nowego trenera z danymi:
-      | imie | nazwisko | wynagrodzenie za godine | typ umowy | waznosc umowy | specjalnosci              |
-      | Jan  | Kowalski | 50                      | B2B       | 25-12-2018    | koszykowka, rehabilitacja |
+    Jeżeli jestem "menadżerem"
+    Gdy gdy dodam nowego trenera z danymi:
+      | imie | nazwisko | umowa                          | specjalnosci              |
+      | Jan  | Kowalski | 50 PLN / h, B2B, do 25-12-2018 | koszykowka, rehabilitacja |
     Wtedy nowy trener powinien pojawić się na liście
     Oraz nowy trener powinien pojawić się w kalendarzu dostępności
     Ale dostępność trenera jest maksymalnie do "25-12-2018"
 
-  Scenariusz: Przypomnienie o wygasającej umowie trenera
-    Zakładając że w rejestrze istnieje trener, którego umowa kończy się "25-10-2018"
-    Oraz jestem menadżerem
-    Wtedy powinienem otrzymać przypomnienie w dniu "25-09-2018"
+  Scenariusz: Modyfikacja danych trenera
+    Jeżeli jestem "menadżerem"
+    Wtedy mogę modyfikować wszystie dane trenera
+    Ale nie mogę modyfikować umowy trenera
 
+  Scenariusz: Przypomnienie o wygasającej umowie trenera
+    Jeżeli jestem "menadżerem"
+    Oraz w rejestrze istnieje trener, którego umowa kończy się "25-10-2018"
+    Wtedy otrzymam przypomnienie w dniu "25-09-2018"
+
+  Scenariusz: Ocena trenera przez klienta siłowni
+    Jeżeli jestem "klientem siłowni"
+    Oraz trener ma średnią ocenę 3.5 wystawioną przez 3 klientów
+    Gdy dodam ocenę 5 z komentarzem "Świetny trener"
+    Wtedy ocena trenera powinna być 3.9
+    Oraz dodana ocena powinna być pierwsza na liście
